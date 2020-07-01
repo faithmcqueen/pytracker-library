c5448e9822e7ea3365b13dbaf6526f4b2dfcc9df
faithmcqueen <faithmcqueen01@gmail.com>
2020-06-30 23:32:37 -0400

    host updated

--- tracker/settings.py
+++ tracker/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
-ALLOWED_HOSTS = ['faithmcqueen-pytrakr.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['infinite-taiga-84300.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
@@ -131,4 +131,3 @@ LOGIN_REDIRECT_URL = 'home'
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-
